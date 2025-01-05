534a09509f4615ce6056217cc47e88df19f082e2
ongaeshi <ongaeshi0621@gmail.com>
2025-01-06 03:29:46 +0900

    feat: Implement for test decode_simplest_module()

--- src/binary.rs
+++ src/binary.rs
@@ -1,1 +1,15 @@
 pub mod module;
+
+#[cfg(test)]
+mod tests {
+    use crate::binary::module::Module;
+    use anyhow::Result;
+
+    #[test]
+    fn decode_simplest_module() -> Result<()> {
+        let wasm = wat::parse_str("(module)")?;
+        let module = Module::new(&wasm)?;
+        assert_eq!(module, Module::default());
+        Ok(())
+    }
+}
--- src/binary/module.rs
+++ src/binary/module.rs
@@ -1,5 +1,35 @@
+use nom::{IResult, number::complete::le_u32, bytes::complete::tag};
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct Module {
     pub magic: String,
     pub version: u32,
+}
+
+impl Default for Module {
+    fn default() -> Self {
+        Self {
+            magic: "\0asm".to_string(),
+            version: 1,
+        }
+    }
+}
+
+impl Module {
+    pub fn new(input: &[u8]) -> anyhow::Result<Module> {
+        let (_, module) =
+            Module::decode(input).map_err(|e| anyhow::anyhow!("failed to parse wasm: {}", e))?;
+        Ok(module)
+    }
+
+    fn decode(input: &[u8]) -> IResult<&[u8], Module> {
+        let (input, _) = tag(b"\0asm")(input)?;
+        let (input, version) = le_u32(input)?;
+
+        let module = Module {
+            magic: "\0asm".into(),
+            version,
+        };
+        Ok((input, module))
+    }
 }
